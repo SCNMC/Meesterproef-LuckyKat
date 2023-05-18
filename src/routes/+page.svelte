820568edff4b3f8713805de17729ec9cf7ee767c
Shauri Maigua <90189750+SCNMC@users.noreply.github.com>
2023-05-18 10:52:59 +0200

    addes uspvideo an changes for uspimages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import HeaderLuckykat from "$lib/components/homepageComponents/HeaderLuckykat.svelte";
 	import Hero from "$lib/components/homepageComponents/Hero.svelte";
 	import Usps from "$lib/components/homepageComponents/Usps.svelte";
+  import Uspsvideo from "$lib/components/homepageComponents/Uspsvideo.svelte";
 	import * as prismicH from "@prismicio/helpers";
 	export let data;
 	const { document } = data;
@@ -11,19 +12,22 @@
 <HeaderLuckykat />
 <Hero />
 <!-- {#each data.document as data} -->
-<Usps textColor='#6A5ACD'
-buttonColor='#FFD700'
+<Usps
+	textColor="#6A5ACD"
+	buttonColor="#FFDB58"
 	uspTitle={document[1].data.title}
 	uspImage={document[1].data.image}
 	uspDescription={document[1].data.description}
 	uspButton={document[1].data.infobutton}
 />
-<Usps textColor='#008080'
-buttonColor='#FF6F61'
+<Usps
+	textColor="#008080"
+	buttonColor="#FF6F61"
 	uspTitle={document[0].data.title}
 	uspImage={document[0].data.image}
 	uspDescription={document[0].data.description}
 	uspButton={document[0].data.infobutton}
 />
+
 
 <!-- {/each} -->
