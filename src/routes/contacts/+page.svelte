32d788897a6c46596d539eee64a9cc8928cdefdd
Shauri Maigua <90189750+SCNMC@users.noreply.github.com>
2023-06-04 21:28:43 +0200

    added colorblocks to footer

--- src/routes/contacts/+page.svelte
+++ src/routes/contacts/+page.svelte
@@ -2,8 +2,10 @@
     	import HeaderLuckykat from "$lib/components/homepageComponents/HeaderLuckykat.svelte";
 		import ContactSection from "$lib/components/contactComponents/ContactSection.svelte";
 		import Footer from "$lib/components/homepageComponents/Footer.svelte";
+		import ColorBlocks from "$lib/components/aboutComponents/ColorBlocks.svelte";
 </script>
 
 <HeaderLuckykat/>
 <ContactSection/>
+<ColorBlocks/>
 <Footer/>
